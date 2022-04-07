9e6f5a83408701d443bbe82340a6bb2edbb10e4f
Tsveti1103 <cvstoqnova@gmail.com>
2022-04-07 15:13:25 +0300

    Create email_validator.py

---
+++ error_handling/email_validator.py
@@ -0,0 +1,34 @@
+class NameTooShortError(Exception):
+    pass
+
+
+class MustContainAtSymbolError(Exception):
+    pass
+
+
+class InvalidDomainError(Exception):
+    pass
+
+
+def is_domain_invalid(current_domain, valid_domains):
+    result = True
+    for valid_domain in valid_domains:
+        if current_domain.endswith(valid_domain):
+            result = False
+            break
+    return result
+
+
+valid_domains = ['.com', '.bg', '.org', '.net']
+while True:
+    email = input()
+    if email == "End":
+        break
+    if "@" not in email:
+        raise MustContainAtSymbolError("Email must contain @")
+    username, domain = email.split("@")
+    if len(username) <= 4:
+        raise NameTooShortError("Name must be more than 4 characters")
+    if is_domain_invalid(domain, valid_domains):
+        raise InvalidDomainError(f"Domain must be one of the following: {', '.join(valid_domains)}")
+    print("Email is valid")
