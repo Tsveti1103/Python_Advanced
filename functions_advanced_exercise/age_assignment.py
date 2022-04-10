079dae4b453898f9238d3916be4b4303caa49a0c
Tsveti1103 <cvstoqnova@gmail.com>
2022-04-10 15:14:24 +0300

    Create age_assignment.py

---
+++ functions_advanced_exercise/age_assignment.py
@@ -0,0 +1,11 @@
+def age_assignment(*args, **kwargs):
+    result = {}
+    for name in args:
+        first_letter = name[0]
+        age = kwargs[first_letter]
+        result[name] = age
+    return result
+
+
+print(age_assignment("Peter", "George", G=26, P=19))
+print(age_assignment("Amy", "Bill", "Willy", W=36, A=22, B=61))
