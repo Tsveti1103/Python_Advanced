02918746b2474219e72088345653d8b1f294ad7e
Tsveti1103 <cvstoqnova@gmail.com>
2022-04-11 15:14:48 +0300

    Create negative_vs_positive.py

---
+++ functions_advanced_exercise/negative_vs_positive.py
@@ -0,0 +1,9 @@
+numbers = [int(el) for el in input().split()]
+negative_sum = sum([x for x in numbers if x < 0])
+positive_sum = sum([x for x in numbers if x > 0])
+print(negative_sum)
+print(positive_sum)
+if abs(negative_sum) > positive_sum:
+    print("The negatives are stronger than the positives")
+else:
+    print("The positives are stronger than the negatives")
